fae0c9e46dc887ed288835c827f842a7030e8f45
Prajoe123 <130102794+Prajoe123@users.noreply.github.com>
2025-07-21 12:03:33 +0100

    Create build.js

---
+++ build.js
@@ -0,0 +1,13 @@
+{
+  "name": "omniprospect",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "dist/index.js",
+  "scripts": {
+    "build": "node build.js",
+    "start": "node dist/index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
